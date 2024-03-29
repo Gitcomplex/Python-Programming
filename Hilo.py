590c3884a8cfbf5f961d495d12ed4ffcf4af2019
Gitcomplex <bu3bounty@gmail.com>
2024-03-29 19:34:34 +0530

    Removed While True condition to low not equal to high and added else to handle normal exit to the loop.

--- Hilo.py
+++ Hilo.py
@@ -6,7 +6,7 @@ input("Please ENTER to start")
 
 guesses = 1
 
-while True:
+while low != high:
     guess = low + (high - low) // 2
     high_low = input(
         f" My guess is {guess} Should I guess higher or lower? Enter h, l or c, If my guess is Correct"
@@ -26,3 +26,7 @@ while True:
         print("Please Enter h, l or c")
 
     guesses += 1
+
+else:
+    print(f"You thought of the number {low}.")
+    print(f"I got it in {guesses} guesses.")
