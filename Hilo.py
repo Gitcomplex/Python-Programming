9ed62fb26dc07f619ef7aaf58811f68e132ef79b
Gitcomplex <bu3bounty@gmail.com>
2024-03-29 18:24:03 +0530

    Created a guessing game using binary search algo

---
+++ Hilo.py
@@ -0,0 +1,28 @@
+low = 1
+high = 1000
+
+print(f"Please think of a number between {low} and {high}: ")
+input("Please ENTER to start")
+
+guesses = 1
+
+while True:
+    guess = low + (high - low) // 2
+    high_low = input(
+        f" My guess is {guess} Should I guess higher or lower? Enter h, l or c, If my guess is Correct"
+    ).casefold()
+
+    if high_low == "h":
+        low = guess + 1
+
+    elif high_low == "l":
+        high = guess - 1
+
+    elif high_low == "c":
+        print(f"I got it {guesses} guesses!")
+        break
+
+    else:
+        print("Please Enter h, l or c")
+
+    guesses += 1
