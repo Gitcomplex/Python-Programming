99a006dde8c41af678e25ac2618a3214ab569ad1
Gitcomplex <bu3bounty@gmail.com>
2024-04-01 20:00:06 +0530

    added lists and tuples folder and lists_intro.py file as well

---
+++ Lists and Tuples/lists_intro.py
@@ -0,0 +1,14 @@
+computer_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat"]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
+
+"""
+Lists and strings behave the same way when it comes to indexing and slicing, since both of them are sequence types
+"""
+
+print(computer_parts[0:3])
+print(computer_parts[-1])
