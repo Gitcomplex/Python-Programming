8902b75c5716b3cddad92411c39e04173e4ca383
Gitcomplex <bu3bounty@gmail.com>
2024-04-04 00:49:29 +0530

    used list comprehension and .replace along with .strip method to effectively clean the output.

---
+++ Lists and Tuples/random_challenge.py
@@ -0,0 +1,38 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+print(flowers)
+
+for plant in data:
+    if "Flower" in plant:
+        flowers.append(plant)
+    else:
+        shrubs.append(plant)
+
+flowers = [item.replace("- Flower", "").strip() for item in flowers]
+shrubs = [item.strip("- Shrub").strip() for item in shrubs]
+
+print(flowers)
+print(shrubs)
