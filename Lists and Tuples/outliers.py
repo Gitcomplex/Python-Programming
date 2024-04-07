9e844d54ac713e1ea28c74608615bfea31d8cdff
Gitcomplex <bu3bounty@gmail.com>
2024-04-07 21:54:35 +0530

    Refactor outliers.py to remove outliers from data list

--- Lists and Tuples/outliers.py
+++ Lists and Tuples/outliers.py
@@ -1,40 +1,58 @@
-data = [
-    4,
-    5,
-    104,
-    105,
-    110,
-    120,
-    130,
-    130,
-    150,
-    160,
-    170,
-    183,
-    185,
-    187,
-    188,
-    191,
-    350,
-    360,
-]
-
 # del data[0:2]
 # print(data)
 # del data[14:]
 # print(data)
 
-min_valid = 100
-max_valid = 200
 
-stop = 0
+def low_values(data, min_valid):
+    stop = 0
 
-for index, value in enumerate(data):
-    if value >= min_valid:
-        stop = index
-        break
+    for index, value in enumerate(data):
+        if value >= min_valid:
+            stop = index
+            break
+
+    del data[:stop]
+    return data
 
 
-print(stop)
-del data[:stop]
-print(data)
+def high_values(data, max_valid):
+    start = 0
+
+    for index in range(len(data) - 1, -1, -1):
+        if data[index] <= max_valid:
+            start = index + 1
+            break
+
+    del data[start:]
+    return data
+
+
+if __name__ == "__main__":
+    data = [
+        4,
+        5,
+        104,
+        105,
+        110,
+        120,
+        130,
+        130,
+        150,
+        160,
+        170,
+        183,
+        185,
+        187,
+        188,
+        191,
+        350,
+        360,
+    ]
+
+    min_valid = 100
+    max_valid = 200
+
+    low_values(data, min_valid)
+    high_values(data, max_valid)
+    print(data)
