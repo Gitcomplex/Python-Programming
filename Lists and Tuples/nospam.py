8fa978595734a1a1c72077fa9b7e4a0642655335
Gitcomplex <bu3bounty@gmail.com>
2024-04-09 02:35:00 +0530

    Refactor nospam.py to remove spam items from menu list

--- Lists and Tuples/nospam.py
+++ Lists and Tuples/nospam.py
@@ -9,3 +9,14 @@ menu = [
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
 ]
 
+# for meal in menu:
+#     for index in range(len(meal) - 1, -1, -1):
+#         if meal[index] == "spam":
+#             del meal[index]
+#     print(meal)
+
+for meal in menu:
+    for item in meal:
+        if item != "spam":
+            print(item)
+    print()
