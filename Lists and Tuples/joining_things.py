378de39872c0f6872a51f4083498ec01f625fc0b
Gitcomplex <bu3bounty@gmail.com>
2024-04-10 00:36:53 +0530

    Add joining_things.py with list of flowers and print the type of joined string

---
+++ Lists and Tuples/joining_things.py
@@ -0,0 +1,19 @@
+flowers = [
+    "Daffodil",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "Lavender",
+    "Lily",
+    "Sunflower",
+    "Tiger Lily",
+]
+
+# for flower in flowers:
+#     print(flower)
+
+# separator = " | "
+# output = separator.join(flowers)
+# print(type(output))
+
+print(type(", ".join(flowers)))
