2ab316e32b64bbfd791e12a187091e252bdb4ba9
Gitcomplex <bu3bounty@gmail.com>
2024-04-10 00:56:22 +0530

    Refactor splitting_things.py to improve list creation and printing

--- Lists and Tuples/splitting_things.py
+++ Lists and Tuples/splitting_things.py
@@ -13,33 +13,13 @@ print(numbers.split(","))
 
 # values = "".join(char if char not in ", " else " " for char in numbers).split()
 
-generated_list = [
-    "9",
-    " ",
-    "2",
-    "2",
-    "3",
-    " ",
-    "3",
-    "7",
-    "2",
-    " ",
-    "0",
-    "3",
-    "6",
-    " ",
-    "8",
-    "5",
-    "4",
-    " ",
-    "7",
-    "7",
-    "5",
-    " ",
-    "8",
-    "0",
-    "7",
-]
+generated_list = ["9"," ",
+                  "2","2","3"," ",
+                  "3","7","2"," ",
+                  "0","3","6"," ",
+                  "8","5","4"," ",
+                  "7","7","5"," ",
+                  "8","0","7"]
 
 values = "".join(generated_list)
 print(values)
