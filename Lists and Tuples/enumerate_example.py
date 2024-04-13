30f209c29355ec2a5f8fb93b112d47667a77ca0b
Gitcomplex <bu3bounty@gmail.com>
2024-04-13 16:29:27 +0530

    Refactor enumerate_example.py to use tuple unpacking for improved readability

--- Lists and Tuples/enumerate_example.py
+++ Lists and Tuples/enumerate_example.py
@@ -1,2 +1,9 @@
-for index, character in enumerate("abcdefgh"):
+# for index, character in enumerate("abcdefgh"):
+#     print(f"{index}: {character}")
+
+for t in enumerate("abcdefgh"):
+    print(t)  # t is a tuple
+
+for t in enumerate("abcdefgh"):
+    index, character = t  # unpacking a tuple
     print(f"{index}: {character}")
