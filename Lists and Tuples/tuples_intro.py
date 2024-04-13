5e2ca72663f5ec44679c8e3714aa91335eda829f
Gitcomplex <bu3bounty@gmail.com>
2024-04-13 16:40:21 +0530

    Refactor tuples_intro.py to use tuple unpacking for improved readability

--- Lists and Tuples/tuples_intro.py
+++ Lists and Tuples/tuples_intro.py
@@ -7,18 +7,31 @@ budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lightning", "Metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
-print()
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
 
-# metallica[0] = "Master of Puppets"
-# tuples uses less memory than lists
-# tuples protect the intergrity of our data
+table = ("Coffee table", 200, 100, 75, 34.50)
+# print(table[1] * table[2])
+# not very readable
 
-metallica2 = list(metallica)
-print(metallica2)
+name, length, width, height, price = table  # unpacking the tuple
+print(length * width)
 
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+# print()
+
+# # metallica[0] = "Master of Puppets"
+# # tuples uses less memory than lists
+# # tuples protect the intergrity of our data
+
+# metallica2 = list(metallica)
+# print(metallica2)
+
+# metallica2[0] = "Master of Puppets"
+# print(metallica2)
