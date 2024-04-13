7f51dfd1557644ade0beab3a554f228f3bbf3656
Gitcomplex <bu3bounty@gmail.com>
2024-04-13 16:02:32 +0530

    Refactor tuples_intro.py to use lists instead of tuples for easier modification

--- Lists and Tuples/tuples_intro.py
+++ Lists and Tuples/tuples_intro.py
@@ -1,3 +1,24 @@
-t = ("a", "b", "c")
-print(type(t))
+# t = ("a", "b", "c")
+# print(type(t))
 
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bud = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+print()
+
+# metallica[0] = "Master of Puppets"
+# tuples uses less memory than lists
+# tuples protect the intergrity of our data
+
+metallica2 = list(metallica)
+print(metallica2)
+
+metallica2[0] = "Master of Puppets"
+print(metallica2)
