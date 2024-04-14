6115c366488352d8ce93b94747111297c2dbc6ae
Gitcomplex <bu3bounty@gmail.com>
2024-04-15 00:29:40 +0530

    Refactor functions.py to add palindrome checking functionality

--- functions_intro/functions.py
+++ functions_intro/functions.py
@@ -3,14 +3,39 @@ def multiply(x, y):
     return result
 
 
-answer = multiply(10.5, 4)
-print(answer)
+def is_palindrome(string):
+    return string[::-1].casefold() == string.casefold()
 
-forty_two = multiply(6, 7)
-print(forty_two)
 
-print()
+def palindrome_sentence(sentence):
+    result = ""
+    for char in sentence:
+        if char.isalnum():
+            result += char
+    return is_palindrome(result)
 
-for val in range(1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+
+# word = input("Please enter a word to check: ")
+# if is_palindrome(word):
+#     print(f"'{word}' is a palindrome!")
+# else:
+#     print(f"'{word}' is not a palindrome.")
+
+sentence = input("Please enter a sentence to check whether it is a paolindrom or not: ")
+if palindrome_sentence(sentence):
+    print(f"'{sentence}' is a palindrome sentence.")
+else:
+    print(f"'{sentence}' is not a palindrome sentence.")
+
+
+# answer = multiply(10.5, 4)
+# print(answer)
+
+# forty_two = multiply(6, 7)
+# print(forty_two)
+
+# print()
+
+# for val in range(1, 5):
+#     two_times = multiply(2, val)
+#     print(two_times)
