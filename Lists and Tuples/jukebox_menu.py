6ec7084d42450c5fd33b40956c4c28ea1dfeb273
Gitcomplex <bu3bounty@gmail.com>
2024-04-14 17:22:53 +0530

    Refactor jukebox_menu.py to display album choices and songs

--- Lists and Tuples/jukebox_menu.py
+++ Lists and Tuples/jukebox_menu.py
@@ -1,7 +1,16 @@
 from neested_data import albums
 
+SONGS_LIST_INDEX = 3
+
 while True:
     print("Please choose your album (invalid choice exists):")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print(f"{index + 1}, {title}, {artist}, {year}, {songs}")
-    break
+        print(f"{index + 1}: {title}")
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice - 1][SONGS_LIST_INDEX]
+    else:
+        break
+
+    print(albums[choice - 1])
+    print(songs_list)
