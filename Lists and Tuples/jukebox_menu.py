8e0d061df081987691ce03f7dcddf33960d7b457
Gitcomplex <bu3bounty@gmail.com>
2024-04-14 17:42:30 +0530

    Refactor jukebox_menu.py to add song selection functionality and improve code readability

--- Lists and Tuples/jukebox_menu.py
+++ Lists and Tuples/jukebox_menu.py
@@ -5,21 +5,23 @@ SONG_TITLE_INDEX = 1
 
 while True:
     print("Please choose your album (invalid choice exists):")
+
     for index, (title, artist, year, songs) in enumerate(albums):
         print(f"{index + 1}: {title}")
+
     choice = int(input())
     if 1 <= choice <= len(albums):
         songs_list = albums[choice - 1][SONGS_LIST_INDEX]
     else:
         break
+
     print("Please choose your song:")
+
     for index, (track_number, song) in enumerate(songs_list):
         print(f"{index + 1}: {song}")
+
     song_choice = int(input())
     if 1 <= song_choice <= len(songs_list):
         title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
-    else:
-        break
-
-    print(f"Playing {title}")
+        print(f"Playing {title}")
     print("=" * 40)
